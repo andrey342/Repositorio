c50aee49123e44163d3ce5b0e32c9834c2be44bd
Andrey <akuzmin@epsg.upv.es>
2021-10-16 18:00:15 +0200

    limpieza proyecto + cambios beacons personalizados

--- src/HolaMundoIBeacon/Publicador.h
+++ src/HolaMundoIBeacon/Publicador.h
@@ -24,7 +24,7 @@ private:
   // ............................................................
 public:
   EmisoraBLE laEmisora {
-	"GTI-3A", //  nombre emisora
+	"Andrey-Kuzmin-G3A", //  nombre emisora
 	  0x004c, // fabricanteID (Apple)
 	  4 // txPower
 	  };
